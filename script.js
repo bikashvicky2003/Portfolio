46428d6de41a41c5d1c44dadb2bf698c0731ffc7
bikashvicky2003 <131902924+bikashvicky2003@users.noreply.github.com>
2023-04-26 23:13:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const typed=new Typed('.multiple-text',{
+    strings: ['Frontend Developer','Coder',' UX Designer'],
+    typespeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
+
